e07155ed31bc6c8b07fcb68236ac39b84787cf3f
hello30190f <w5w5ww6w6w@gmail.com>
2022-06-30 16:04:16 +0900

    I added "A - Last Letter.cpp".

---
+++ AtCoder Beginner Contest 244/A - Last Letter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int count;
+    cin >> count;
+    string ch;
+    cin >> ch;
+    for(int i = 0;i < count;i++){
+        if(count - 1 == i){
+            cout << ch[count - 1] <<endl;
+        }else{}
+
+        }
+    return 0;
+    }
