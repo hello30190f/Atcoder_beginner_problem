0079ef2f2576934cde6a6ccba3adbedbda65ab47
hello30190f <108202763+hello30190f@users.noreply.github.com>
2022-07-07 19:04:55 +0900

    I add code which divide x and y element.

--- 246/Four Points/Solve.cpp
+++ 246/Four Points/Solve.cpp
@@ -4,6 +4,27 @@ using namespace std;
 using namespace std;
 
 int main() {
+    int position1[6];
+    int odd[3]; // y
+    int even[3]; // x
+    // int position2[2];
+    // int position3[2];
 
+    for(int i = 0; i < 6; i++){
+        cin >> position1[i];
+        // cout << position1[i] << endl;
+    }
 
+    for(int i = 0; i < 3; i++){
+        
+        even[i] = position1[i * 2];
+        cout << even[i] << endl;
+        // cout << position1[i] << endl;
+    }
+
+        for(int i = 0; i < 3; i++){
+        odd[i] = position1[i * 2 + 1];
+        cout << odd[i] << endl;
+        // cout << position1[i] << endl;
+    }
 }
